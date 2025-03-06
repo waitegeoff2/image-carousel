ed7c1f0f90bc9e2855121fd1ddf2ef5b63111e29
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-06 10:54:28 -0500

    commented out code for multiple slideshows

--- src/index.js
+++ src/index.js
@@ -46,14 +46,37 @@ prevButton.addEventListener("click", () => {
 // let slideIndex = 0;
 // showSlides();
 
-// function showSlides() {
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++; //change index automatically
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // RUN showslides every 2 seconds, index goes up by one each time
+}
+
+//MULTIPLE SLIDESHOWS
+
+// let slideIndex = [1,1];
+// /* Class the members of each slideshow group with different CSS classes */
+// let slideId = ["mySlides1", "mySlides2"]
+// showSlides(1, 0);
+// showSlides(1, 1);
+
+// function plusSlides(n, no) {
+//   showSlides(slideIndex[no] += n, no);
+// }
+
+// function showSlides(n, no) {
 //   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
+//   let x = document.getElementsByClassName(slideId[no]);
+//   if (n > x.length) {slideIndex[no] = 1}
+//   if (n < 1) {slideIndex[no] = x.length}
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
 //   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+//   x[slideIndex[no]-1].style.display = "block";
 // }
