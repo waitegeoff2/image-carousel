122ff0cac6cc5b6b8bff6edcdbc179c68346cc16
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-03-06 10:54:56 -0500

    commented code for auto slide show

--- src/index.js
+++ src/index.js
@@ -46,17 +46,17 @@ prevButton.addEventListener("click", () => {
 // let slideIndex = 0;
 // showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++; //change index automatically
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // RUN showslides every 2 seconds, index goes up by one each time
-}
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++; //change index automatically
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // RUN showslides every 2 seconds, index goes up by one each time
+// }
 
 //MULTIPLE SLIDESHOWS
 
